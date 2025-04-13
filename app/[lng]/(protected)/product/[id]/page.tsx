aa46dae302a3c2316bcb1f73b105af715edb210e
sapovnela es <eralda.shullani@cit.edu.al>
2025-04-13 03:34:52 +0200

    produkte

---
+++ app/[lng]/(protected)/product/[id]/page.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Product from '@/components/product/Product'
+
+const ProductPage = () => {
+    return (
+        <div><Product /></div>
+    )
+}
+
+export default ProductPage
